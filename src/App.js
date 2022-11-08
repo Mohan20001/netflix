7f6e225498a38d9b0f7d743a9dc0ff7c62fa0e2f
mohan putta <puttamohan2001@gmail.com>
2022-11-08 18:54:54 +0530

    hero addaed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
     //  row componet   
     // 3a14c7390230b3a5a2dba32ee4278cb2
     <>
-    <Hero></Hero>
+    <Hero/>
+    <div className="Content-section" >
     <Row title="Popular TV Shows" url="https://api.themoviedb.org/3/tv/popular?api_key=3a14c7390230b3a5a2dba32ee4278cb2&language=en-US&page=1"></Row>
     <Row title="Trending Today" url="https://api.themoviedb.org/3/trending/all/day?api_key=3a14c7390230b3a5a2dba32ee4278cb2"></Row>
     <Row title="TV Airing Today" url="https://api.themoviedb.org/3/tv/airing_today?api_key=3a14c7390230b3a5a2dba32ee4278cb2&language=en-US&page=1"></Row>
@@ -19,6 +20,7 @@ function App() {
     <Row title="Popular Movies" url="https://api.themoviedb.org/3/movie/popular?api_key=3a14c7390230b3a5a2dba32ee4278cb2&language=en-US&page=1"></Row>
     <Row title="Top Rated Movies" url="https://api.themoviedb.org/3/movie/top_rated?api_key=3a14c7390230b3a5a2dba32ee4278cb2&language=en-US&page=1"></Row>
     <Row title="Upcoming Movies" url="https://api.themoviedb.org/3/movie/upcoming?api_key=3a14c7390230b3a5a2dba32ee4278cb2&language=en-US&page=1"></Row>
+    </div>
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,11 +3,14 @@ import "../componetStyles/heroStyle.css";
 
 const Hero = ()=> {
    return(
-     <>
-     <video className="hero-video" autoPlay>
-        <source src="https://www.youtube.com/embed/zJwbZX4i-N4"></source>
-     </video>
-     </>
+     <div className="Hero">
+        <h1 className="Movie-title">Movie Name part 2 avalable</h1>
+        <p className="Movie-description">Movie Description in some length of sentancesLorem ipsum is a dummy text without any sense. It is a sequence of Latin words that</p>
+        <div className="btn-container">
+         <button className="Play-btn">Play Now</button>
+         <button className="Info-btn">More Info</button>
+        </div>
+     </div>
    );
 }
 

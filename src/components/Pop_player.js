064259bdc0d91337fcdabce3b81e702c0b26dc73
mohan putta <puttamohan2001@gmail.com>
2022-11-17 12:11:54 +0530

    pop player open/close functionality

--- src/components/Pop_player.js
+++ src/components/Pop_player.js
@@ -5,14 +5,15 @@ import '../componetStyles/Pop_player.css';
 const Pop_player = (props)=>{
   // console.log(props.v_url);
   let video_url = `https://www.youtube.com/watch?v=${props.v_url}`;
-  let x = true;
-  if(x == true){
+
+  let [y, setY] = useState(true);
+  if(y == true){
     return(
       <div className="preview-container">
           <div className="pop-player">
             <div className="video-wrapper">
                <ReactPlayer className="pop-video" url={video_url} width="100%" height="100%" loop={false} autoPlay={true}/>
-               <span className="close-btn-top" onClick={()=> x = false}>EXITE</span>
+               <span className="close-btn-top" onClick={()=> setY(false)}>EXITE</span>
             </div>
             <div>
 
